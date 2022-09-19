a0427d2e22f9317ce802356caae8b5ed71dbe168
Ernesto Rojas <ejrojasp@falabella.cl>
2022-09-19 18:05:30 -0300

    Added folder public and example of Handlebars.

--- class-9/resume-class-8/index.js
+++ class-9/resume-class-8/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const routers = require('./routers')
 const { errorHandler } = require('./utils/errores')
 
@@ -8,6 +9,9 @@ const app = express()
 const PORT = process.env.NODE_PORT
 const ENV = process.env.NODE_ENV
 
+app.use(cors())
+
+app.use('/usuarios/', express.static(path.join(__dirname, 'public/')))
 app.use('/api/usuarios/avatares', express.static(path.join(__dirname, 'pictures/')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
