6a3ac3657b0e8c18cbe47714e4892ba8056cb6e4
Ernesto Rojas <ejrojasp@falabella.cl>
2022-10-04 03:09:35 -0300

    Added Top Level Async Await line.

--- class-13/examples/handlerbars-front/app.js
+++ class-13/examples/handlerbars-front/app.js
@@ -11,6 +11,8 @@ const PORT = process.env.NODE_PORT || 3000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+console.log(await Promise.resolve('Configurando servidor...'))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
